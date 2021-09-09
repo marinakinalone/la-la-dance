e83a9f4e3027f90ca5340d69b18d9416a8be04ad
marinakinalone <kinalone.dev@gmail.com>
2021-09-09 10:05:44 +0200

    set up clickable interface for questionnaire with progress bar (no layout nor functional atm)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {questionsList} from './questions.js';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Questionnaire />
+      <Footer />
     </div>
   );
+}
+class Questionnaire extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questionNb: 0,
+      progress: 5,
+      score: 0
+    }
+
+    this.next = this.next.bind(this)
+
+  }
+
+  next() {
+    this.setState({
+      questionNb: this.state.questionNb + 1,
+      progress: ((this.state.questionNb + 1) * 100) / (questionsList.length - 1)
+    })
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h2>{questionsList[this.state.questionNb].question}</h2>
+        <button onClick={this.next}>{questionsList[this.state.questionNb].answerOne}</button>
+        <button onClick={this.next}>{questionsList[this.state.questionNb].answerTwo}</button>
+        <button onClick={this.next}>{questionsList[this.state.questionNb].answerThree}</button>
+        <button onClick={this.next}>{questionsList[this.state.questionNb].answerFour}</button>
+        {/*!let's not forget progress bar!*/}
+        <ProgressBar now={this.state.progress} />
+      </div>
+      
+
+    )
+  }
+}
+class Header extends React.Component{
+  render() {
+    return (
+      <header className="title">
+        <h1>La La Dance</h1>
+        <p className="header_quote">"Dance, dance, otherwise we are lost" (Pina Bausch)</p>
+      </header>
+    )
+  }
+}
+class Footer extends React.Component{
+  render() {
+    return (
+      <footer>
+        <p className="copyright">Made while dancing by <a href="https://kinalone.dev" target="_blank" rel="noopener noreferrer">MKS</a> | <a href="../license.txt" target="_blank" rel="noopener noreferrer">license</a></p>
+      </footer>
+    )
+  }
 }
 
 export default App;
---
+++ src/questions.js
@@ -0,0 +1,50 @@
+const questionsList = [
+    {
+    question: "What kind of music do you prefer?",
+    answerOne: "Latin",
+    scoreOne: [],
+    answerTwo: "Pop",
+    scoreTwo: [],
+    answerThree: "Jazz",
+    scoreThree: [],
+    answerFour: "Electro",
+    scoreFour: []
+    },
+    {
+    question: "What kind of beat do you like?",
+    answerOne: "Upbeat",
+    scoreOne: [],
+    answerTwo: "Walk-paced",
+    scoreTwo: [],
+    answerThree: "Slow",
+    scoreThree: [],
+    answerFour: "I like everything",
+    scoreFour: []
+    },
+    {
+    question: "What kind of music do you prefer?",
+    answerOne: "Latin",
+    scoreOne: [],
+    answerTwo: "Pop",
+    scoreTwo: [],
+    answerThree: "Jazz",
+    scoreThree: [],
+    answerFour: "Electro",
+    scoreFour: []
+    },
+    {
+    question: "last",
+    answerOne: "Latin",
+    scoreOne: [],
+    answerTwo: "Pop",
+    scoreTwo: [],
+    answerThree: "Jazz",
+    scoreThree: [],
+    answerFour: "Electro",
+    scoreFour: []
+    }
+];
+
+
+
+export {questionsList}
