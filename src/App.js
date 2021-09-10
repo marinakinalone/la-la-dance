20146a155132a62a3505ab8df33a1d5ff381bf13
marinakinalone <kinalone.dev@gmail.com>
2021-09-10 18:19:11 +0200

    conditional display from intro to test, and from result to intro

--- src/App.js
+++ src/App.js
@@ -16,37 +16,66 @@ class App extends React.Component{
       progress: 5,
       score: 0,
       color: colors[0],
-      result: 'chachacha'
+      result: 'chachacha',
+      displayTest: false,
+      displayIntro: true
 
     }
 
     this.next = this.next.bind(this)
+    this.handleClickToTest = this.handleClickToTest.bind(this)
+    this.handleClickToIntro = this.handleClickToIntro.bind(this)
 
   }
 
   next() {
-    this.setState({
+    this.setState({ 
       questionNb: this.state.questionNb + 1,
       progress: ((this.state.questionNb + 1) * 100) / (questionsList.length - 1),
       color: colors[this.state.questionNb + 1 ]
     })
+   
   }
 
+  handleClickToTest() {
+    this.setState({
+      displayTest: true,
+      displayIntro: false
+    })
+  }
+
+  handleClickToIntro() {
+    this.setState({
+      displayTest: false,
+      displayIntro: true
+    })
+  }
+
+
+
   render() {
+    let mainContent = <Introduction style={{color: this.state.color, borderColor: this.state.color}} onClick={this.handleClickToTest}/>;
+
+    if (this.state.displayTest) {
+      mainContent = <div className="test">
+      <Questionnaire question={questionsList[this.state.questionNb].question} />
+      {questionsList[this.state.questionNb].answers.map(answer => (
+      <button onClick={this.next} style={{color: this.state.color, borderColor: this.state.color}}>
+        {answer}
+      </button>
+    ))}
+      <ProgressBar now={this.state.progress} style={{backgroundColor: this.state.color, color: this.state.color, height: "4px"}}/>
+      </div>
+    }
+
+    // if (this.state.questionNb == (questionsList.length)) {
+    //   mainContent = <Result result={this.state.result} style={{color: this.state.color, borderColor: this.state.color}} onClick={this.handleClickToIntro}/>
+    // }
+
     return (
       <div style={{color: this.state.color}}>
         <Header />
-        <Introduction style={{color: this.state.color, borderColor: this.state.color}} />
-        <div className="test">
-        <Questionnaire question={questionsList[this.state.questionNb].question} />
-        {questionsList[this.state.questionNb].answers.map(answer => (
-        <button onClick={this.next} style={{color: this.state.color, borderColor: this.state.color}}>
-          {answer}
-        </button>
-      ))}
-        <ProgressBar now={this.state.progress} style={{backgroundColor: this.state.color, color: this.state.color, height: "4px"}}/>
-        </div>
-        <Result result={this.state.result} style={{color: this.state.color, borderColor: this.state.color}}/>
+        {mainContent}
         <Footer />
       </div>
     )
@@ -69,7 +98,7 @@ class Introduction extends React.Component {
     return (
       <div>
         <p>description</p>
-        <button style={this.props.style}>I want to dance</button>
+        <button style={this.props.style} onClick={this.props.onClick}>I want to dance</button>
         <figure>
         <img src={illustration} alt="extract from the movie La La Land where a woman with a yellow dress dances with a man with a black and white costume in front of a night-time landscape with blue and purple colors"></img>
         <figcaption>source: wallpaperflare.com</figcaption>
@@ -85,7 +114,7 @@ class Result extends React.Component {
       <div>
         <h2 style={this.props.color}>Your social dance style is: </h2>
         <p className="result">{this.props.result}</p>
-        <button style={this.props.style}>take the questionnaire again</button>
+        <button style={this.props.style} onClick={this.props.onClick}>take the questionnaire again</button>
       </div>
     )
   }
