ff650c625cc0b0fbcfd2965ebc4594335d7c93fe
marinakinalone <kinalone.dev@gmail.com>
2021-09-29 16:39:22 +0200

    tested successful process to result update within questions.js before implementing in App.js

--- src/questions.js
+++ src/questions.js
@@ -2,7 +2,12 @@ const questionsList = [
     {
     question: "What kind of music do you prefer?",
     answers: ["Latin", "Pop", "Jazz", "Electro"],
-    score: 0
+    score: {
+        "Latin": ["Salsa", "Brazilian Zouk", "ArgentineTango"],
+        "Pop": ["West Coast Swing", "Fusion"],
+        "Jazz": ["Lindy Hop", "Blues"],
+        "Electro": ["West Coast Swing", "Lindy Hop", "Fusion"]
+        }
     },
     {
     question: "What kind of beat do you prefer?",
@@ -29,10 +34,36 @@ const questionsList = [
     answers: ["Feel the present moment", "Self-expression", "Energy", "Sensuality"],
     score: 0
     }
-
     
 ];
 
 
+var results = {
+    "West Coast Swing": 0,
+    "Lindy Hop": 0,
+    "Salsa": 0,
+    "Brazilian Zouk": 0,
+    "Fusion": 0,
+    "Blues": 0,
+    "Argentine Tango": 0}
+
+
+const updateScore = (questionNb, answer) => {
+    let keysToScore = questionsList[questionNb].score[answer];
+    keysToScore.forEach(key => {
+        results[key] = results[key] + 1
+    } )
+    return results
+}
+
+console.log(updateScore(0, "Jazz"))
+console.log(updateScore(0, "Jazz"))
+
+const testTest = (tolog) => {
+    return "hey this is the result " + tolog
+}
+
+
 
 export {questionsList}
+export {testTest}
