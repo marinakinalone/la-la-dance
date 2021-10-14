cfc7befa444ed98e49a8efaf46af96c8babe8fd7
marinakinalone <kinalone.dev@gmail.com>
2021-10-14 10:39:25 +0200

    implemented another try to fix sticky hover effect in mobile devices

--- src/App.js
+++ src/App.js
@@ -11,7 +11,10 @@ import {updateScore} from './questionsAndScoreHandling.js';
 import {getFinalScore} from './questionsAndScoreHandling.js';
 const colors = ["#FF6200", "#00DFDC", "#FF2E8A", "#00FF7F", "#FFE641", "#F278FF"]
 
-
+var touchsupport = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0)
+if (!touchsupport){ // browser doesn't support touch
+    document.documentElement.className += " non-touch"
+}
 class App extends React.Component{
   constructor(props) {
     super(props);
